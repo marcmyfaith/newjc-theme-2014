64c07eb61ab9290a371bdb17dabb0e3f00da76eb
Marcus Battle <marcus@marcusbattle.com>
2014-05-24 19:06:04 -0400

    major updates

--- home.php
+++ home.php
@@ -11,60 +11,66 @@
             <div class="row">
                <div class="col-md-12 text-center">
                   <h1>An Innovative Church That Provokes the Culture of the Kingdom of God</h1>
-                  <p><a class="btn btn-primary btn-lg" role="button" href="<?php echo home_url(); ?>/homelessness">Learn more</a></p>
+                  <p>&nbsp;</p>
+                  <p><a class="btn btn-warning btn-lg" role="button" href="<?php echo home_url(); ?>/homelessness">Learn more</a></p>
                </div>
             </div>
          </div>
       </div>
 
       <div class="filter"></div>
-   </header><!-- #masthead -->
+   </header><!-- #header -->
 
-   <?php if ( is_plugin_active( 'lemonbox-events/lemonbox-events.php' ) ): ?>
-      
-      <?php $upcoming_event = get_next_event(); ?>
-      <?php if ( $upcoming_event ): ?>
-         <div id="upcoming-event" class="container-fluid">
-            <div class="container">
-               <div class="row">
-                  <div class="col-sm-5 col-md-5 col-md-offset-1">
-                     <h2>
-                        <a href="<?php echo $upcoming_event->permalink ?>"><?php echo $upcoming_event->post_title ?></a>
-                        <br /><small><?php echo $upcoming_event->date ?></small>
-                     </h2>
-                     <p><a href="<?php echo home_url(); ?>/events">View all events</a></p>
-                  </div>
-                  <div class="col-sm-4 col-md-3 col-sm-7 col-xs-12">
-                      <h4>Event Begins In</h4>
-                     <div class="event-countdown event-countdown-small" data-date="<?php echo $upcoming_event->date ?>" style="display: none;">
-                        <div class="timer-col"><span id="days" class="label label-primary"></span><span class="timer-type">days</span></div>
-                        <div class="timer-col"><span id="hours" class="label label-default"></span><span class="timer-type">hrs</span></div>
-                        <div class="timer-col"><span id="minutes" class="label label-default"></span><span class="timer-type">mins</span></div>
-                        <div class="timer-col"><span id="seconds" class="label label-default"></span><span class="timer-type">secs</span></div>
-                     </div>
-                  </div>
-                   <div class="col-xs-12 col-sm-3 col-md-2 col-xs-5">
-                     <?php if( $upcoming_event->ticket_id ): ?>
-                        <a type="button" class="btn btn-lg btn-primary" href="<?php echo $upcoming_event->permalink ?>">Buy Ticket $<?php echo isset($upcoming_event->ticket->meta['product_price'][0]) ? round($upcoming_event->ticket->meta['product_price'][0]) : '' ?></a>
-                     <?php else: ?>
-                        <a type="button" class="btn btn-primary btn-lg" style="margin-top: 29px;">View Event</a>
-                     <?php endif; ?>
-                  </div>
-               </div>
+   <?php echo do_shortcode('[upcoming_event]'); ?>
+
+   <div id="quick-links" class="container-fluid">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-7">
+               <h2>First Time Visitors</h2>
+               <p>Visiting anywhere by yourself for the first time can be pretty intimidating. We want to make sure your visit to any one of our services exceeds your expectations. To do that, we have prepared this quick overview of what you can expect upon arrival.</p>
+               <p><a class="btn btn-warning">Welcome Home</a></p>
+               <p>&nbsp;</p>
+               <h2>Get Connected</h2>
+               <p>With a church that serves over 3,000 members and hundreds of guests each week, one can easily get lost in the crowd. To remedy this, we connect members with opportunities to serve God and our city by creating a close-knit community of believers.</p>
+               <p><a class="btn btn-warning">Find Your Calling</a></p>
+            </div>
+            <div class="col-md-4 col-md-offset-1">
+               <?php dynamic_sidebar( 'Sidebar' ); ?>
             </div>
          </div>
-      <?php endif; ?>
-   <?php endif; ?>
+      </div>
 
-   <div class="content container-fluid">
+      <div class="filter"></div>
+   </div>
+
+   <div id="main" class="container-fluid">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-4">
+               <div class="thumbnail">
+                  <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/dr-kevin-williams-square.jpg' ?>" />
+               </div>
+            </div>
+            <div class="col-md-8">
+               <h2>Dr. Kevin Williams <small>Senior Pastor</small></p>
+               <p class="lead">Dr. Williams is a prolific speaker, teacher, preacher, and eductor. He is an author of rising acclaim and the expert of turning points, with his new book release, "Turning Point".</p>
+               <p><a href="<?php echo home_url( 'leadership' ); ?>" class="">View Church Leadership</a> | <a href="https://twitter.com/DrKAWilliams">Follow on Twitter</a></p>
+            </div>
+         </div>
+      </div>
+   </div>
+
+   <div id="main" class="content container-fluid hide">
       <div class="container">
          <div class="row">
             <div class="col-md-8">
                <h2>Latest News</h2>
+               <p>&nbsp;</p>
                <?php get_template_part( 'parts/post/loop' ); ?>
             </div>
             <div class="col-md-3 col-md-offset-1">
-               <?php dynamic_sidebar( 'Right Sidebar' ); ?>
+               
             </div>
          </div>
       </div>
