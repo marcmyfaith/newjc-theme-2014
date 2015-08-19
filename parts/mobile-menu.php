2ad4fdf64f78e09b849eb21384404ebbd23c9cf0
Marcus Battle <marcusbattle@Marcbook.local>
2015-08-19 02:24:22 -0400

    Modified mobile menu

---
+++ parts/mobile-menu.php
@@ -0,0 +1,22 @@
+<div id="mobile-header" class="">
+	<?php if ( get_theme_mod( 'stencil_logo' ) ) : ?>
+       <a class="logo" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo get_stylesheet_directory_uri(). '/assets/img/newjc-logo-small.png' ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
+    <?php else : ?>
+       <div class="title">
+          <h1 class="site-title"><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
+          <span class="site-description"><?php bloginfo( 'description' ); ?></span>
+       </div>
+    <?php endif; ?>
+	<a class="mobile-menu-trigger" href="#">Menu</a>
+</div>
+
+<div id="mobile-menu" class="slide">
+	<?php wp_nav_menu( array( 
+		'theme_location' => 'header-menu', 
+		'container' => 'false',
+		'menu_class' => 'nav nav-pills nav-stacked', 
+		'walker' => new HeaderMenuWalker 
+	) ); ?>
+</div>
+
+<div class="mobile-menu-overlay"></div>
