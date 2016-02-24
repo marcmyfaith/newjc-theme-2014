4c1c78441972b3bef065021bfab8636506800a5a
root <root@east.battleideas.xyz>
2016-02-24 00:49:08 -0500

    Added google site verification

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
-	
+
 	function newjc_scripts() {
 		wp_enqueue_style( 'newjc', get_stylesheet_directory_uri() . '/assets/css/newjc.css', array( 'bootstrap', 'stencil' ) );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'newjc_scripts' );
 
-?>
+	function newjc_google_site_verification() {
+		echo '<meta name="google-site-verification" content="oMN3yEb12-VgQvdMeiPUjl09yt3CDiRby8ny0hQ_eI8" />';
+	}
+
+	add_action( 'wp_head', 'newjc_google_site_verification' );
